43ea5f3a4a484f12365aeedc4e9cf1cb32fed394
Larry Eliemenye <denachural@gmail.com>
2016-05-27 09:40:57 +0100

    More gulp stuff(1)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require('gulp-help')(require('gulp'));
 var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 
-gulp.task('compile:css','compiles scss files to css', function(){
+gulp.task('compile:sass', 'compiles scss files to css', function () {
+  return gulp.src('./scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(gulp.dest('./public/css/app.css'))
+});
+
+gulp.task('watch:sass', 'starts a watch task on your scss files', function () {
+  gulp.watch('./scss/**/*.scss', ['compile:css']);
+});
+
+gulp.task('compress:app-css', 'conditionally compresses your css files based on environment', function () {
+  return gulp.src('./public/css/app.css')
+      .pipe(gutil.env.NODE_ENV === 'prod' ? cssmin() : gutil.noop())
+      .pipe(rename('app.min.css')).pipe('./public/css')
+}, {
+  options: {
+    "env=dev": "set the environment 'compress:css' will run to change it's behaviour"
+  }
 });
